8a867ce498b31f13f7c02ae5a063c7bd1a2f0d09
wagishsharma <wagishsharma2@gmail.com>
2016-05-12 23:47:13 +0530

    Initial file

---
+++ Assets/Scripts/PlayerComponent.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerComponent : MonoBehaviour {
+	private int count;
+	public int speed;
+	private Rigidbody2D rb2d;
+	public Text countText;
+	public Text winText;
+	// Use this for initialization
+	void Start () 
+	{	count = 0;
+		rb2d = GetComponent<Rigidbody2D> ();
+		winText.text = "";
+		setCountText ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+
+		
+
+	}
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			//... then set the other object we just collided with to inactive.
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			setCountText ();
+			if (count >= 8)
+			{
+				winText.text = "You Win !";
+
+			}
+		}
+	}
+	void setCountText(){
+
+		countText.text = "Count :" + count; 
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, 15) * Time.deltaTime);
+
+	}
+}
